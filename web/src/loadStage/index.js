8078cd7d11a0265894cd92e33b14fce3b838383c
aaronkadima <aaronkadima@gmail.com>
2026-09-13 20:04:22 -0300

    feat: expose v0.40 load stage engine

---
+++ web/src/loadStage/index.js
@@ -0,0 +1,8 @@
+export * from './actionEngine.js';
+export * from './movingLoad.js';
+export * from './constructionStages.js';
+export * from './prestress.js';
+export * from './timeDependent.js';
+
+export const LOAD_STAGE_ENGINE_CONTRACT='load-stage-engine/v1';
+export const LOAD_STAGE_ENGINE_VERSION='0.40.0-exp';

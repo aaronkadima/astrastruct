29e87eebf9cadfd0974515985576eaed09c9d7ff
aaronkadima <aaronkadima@gmail.com>
2026-09-13 20:40:28 -0300

    feat: export v0.42 design actions API

---
+++ web/src/designActions/index.js
@@ -0,0 +1,10 @@
+export {
+  DESIGN_ACTIONS_CONTRACT,
+  LOAD_COMBINATIONS_CONTRACT,
+  COMBINATION_ENVELOPE_CONTRACT,
+  DESIGN_ACTIONS_VERSION,
+  defineDesignActions,
+  generateDesignCombinations,
+  compileDesignCombination,
+  envelopeCombinationResults
+} from './combinationEngine.js';

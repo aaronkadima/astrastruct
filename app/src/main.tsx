09447990a4ab08d2d492de4a0b3cc356aab5faa0
aaronkadima <aaronkadima@gmail.com>
2026-09-13 12:33:50 -0300

    ui: load adaptive shell4 refinement controls

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -26,6 +26,7 @@ import './levelsBootstrap';
 import './shellLabBootstrap';
 import './shellMeshBootstrap';
 import './shellRefinementBootstrap';
+import './shellAdaptiveBootstrap';
 import './shellQualityBootstrap';
 import './shellConvergenceBootstrap';
 import './shellQualityResultsBootstrap';

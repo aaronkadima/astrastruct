33137d01cb743f4d9db51a715455ba94f1bada23
aaronkadima <aaronkadima@gmail.com>
2026-09-13 18:25:33 -0300

    feat: expose Section Engine API

---
+++ web/src/sections/index.js
@@ -0,0 +1,7 @@
+export * from './geometry.js';
+export * from './fibers.js';
+export * from './materialLaws.js';
+export * from './sectionResponse.js';
+export * from './builders.js';
+
+export const SECTION_ENGINE_CONTRACT='section-engine/v1';

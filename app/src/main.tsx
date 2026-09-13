6e66e7e8bda05d6632cdbfa32c27910a700f84c1
aaronkadima <aaronkadima@gmail.com>
2026-09-13 13:34:34 -0300

    ui: load nonlinear bolt contact laboratory

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -24,6 +24,7 @@ import './modelLabBootstrap';
 import './advancedExamplesBootstrap';
 import './anchorLabBootstrap';
 import './boltGroupLabBootstrap';
+import './boltContactLabBootstrap';
 import './punchingLabBootstrap';
 import './labStatusBootstrap';
 import './dxfPlanBootstrap';

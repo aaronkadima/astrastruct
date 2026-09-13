c1843032a0aaf1350909c7823dc74b5170550833
aaronkadima <aaronkadima@gmail.com>
2026-09-13 20:31:57 -0300

    feat: export v0.41 nonlinear dynamics API

---
+++ web/src/dynamics/index.js
@@ -0,0 +1,11 @@
+export {
+  NONLINEAR_DYNAMICS_CONTRACT,
+  NONLINEAR_DYNAMICS_VERSION,
+  GROUND_MOTION_CONTRACT,
+  rayleighDampingMatrix,
+  piecewiseLinearGroundMotion,
+  baseExcitationForce,
+  absoluteAcceleration,
+  createComponentRestoringModel,
+  newmarkNonlinearSystem
+} from './nonlinearTransient.js';

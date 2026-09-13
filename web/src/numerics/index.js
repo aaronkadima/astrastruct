774cff4d1e4c05dd77d2d1479e8240e55a0e7294
aaronkadima <aaronkadima@gmail.com>
2026-09-13 18:00:46 -0300

    feat: expose Numerical Core 2 public surface

---
+++ web/src/numerics/index.js
@@ -0,0 +1,7 @@
+export * from './diagnostics.js';
+export * from './sparseMatrix.js';
+export * from './linearSolver.js';
+export * from './dofManager.js';
+export * from './constraints.js';
+export * from './units.js';
+export * from './nonlinearSolver.js';

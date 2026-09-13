b8ec5f0f49ad18db966147474a0c187dcdfa1b5d
aaronkadima <aaronkadima@gmail.com>
2026-09-13 19:41:21 -0300

    feat: export node-to-surface contact primitives

--- web/src/shellContact/index.js
+++ web/src/shellContact/index.js
@@ -2,6 +2,7 @@ export * from '../shell/concretePlaneStressFixedCrack.js';
 export * from '../shell/layeredShellSection.js';
 export * from '../shell/nonlinearShell4Component.js';
 export * from '../contact/contactPair.js';
+export * from '../contact/nodeSurfaceContact.js';
 export * from './registry.js';
 
 export const ADVANCED_SHELL_CONTACT_CONTRACT='advanced-shell-contact/v1';

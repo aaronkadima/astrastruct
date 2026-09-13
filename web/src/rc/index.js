e454ad9b851be3f24da76883ebde43311cf9307c
aaronkadima <aaronkadima@gmail.com>
2026-09-13 19:18:14 -0300

    feat: export v0.37 RC registry

--- web/src/rc/index.js
+++ web/src/rc/index.js
@@ -3,6 +3,7 @@ export * from './rebar1d.js';
 export * from './bondSlip1d.js';
 export * from './sectionResponse.js';
 export * from './bondLinkComponent.js';
+export * from './registry.js';
 
 export const NONLINEAR_RC_CONTRACT='nonlinear-rc/v1';
 export const NONLINEAR_RC_VERSION='0.37.0-exp';

d39685bdb0a3b76114ddec9d50f5df26e0518795
aaronkadima <aaronkadima@gmail.com>
2026-09-13 19:34:00 -0300

    feat: export v0.38 shell-contact library

---
+++ web/src/shellContact/index.js
@@ -0,0 +1,8 @@
+export * from '../shell/concretePlaneStressFixedCrack.js';
+export * from '../shell/layeredShellSection.js';
+export * from '../shell/nonlinearShell4Component.js';
+export * from '../contact/contactPair.js';
+export * from './registry.js';
+
+export const ADVANCED_SHELL_CONTACT_CONTRACT='advanced-shell-contact/v1';
+export const ADVANCED_SHELL_CONTACT_VERSION='0.38.0-exp';

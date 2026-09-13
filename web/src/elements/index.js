ff8cedd99232cd030210eb10615d382ebcfe567b
aaronkadima <aaronkadima@gmail.com>
2026-09-13 18:54:59 -0300

    feat: export Advanced Element Library v0.36

---
+++ web/src/elements/index.js
@@ -0,0 +1,9 @@
+export * from './timoshenko2d.js';
+export * from './timoshenko3d.js';
+export * from './cable.js';
+export * from './nonlinearLink.js';
+export * from './rigidOffsetFrame.js';
+export * from './registry.js';
+
+export const ADVANCED_ELEMENT_LIBRARY_CONTRACT='advanced-element-library/v1';
+export const ADVANCED_ELEMENT_LIBRARY_VERSION='0.36.0-exp';

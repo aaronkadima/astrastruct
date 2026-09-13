ed6d9de2092cad4d4fca63285328625b4dd88469
aaronkadima <aaronkadima@gmail.com>
2026-09-13 19:53:43 -0300

    feat: expose v0.39 connections anchors API

---
+++ web/src/connections/index.js
@@ -0,0 +1,8 @@
+export * from './connectionObject.js';
+export * from './localGlobalCoupling.js';
+export * from './connectionComponent.js';
+export * from './anchorGroup3d.js';
+export * from './registry.js';
+
+export const CONNECTIONS_ANCHORS_CONTRACT='connections-anchors/v1';
+export const CONNECTIONS_ANCHORS_VERSION='0.39.0-exp';

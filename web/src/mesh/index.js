4ae2e8ad199e50ce68ecbf2092a381e2fa9975cc
aaronkadima <aaronkadima@gmail.com>
2026-09-13 18:44:16 -0300

    feat: export Mesh Surface Engine v0.35 API

---
+++ web/src/mesh/index.js
@@ -0,0 +1,9 @@
+export * from './meshModel.js';
+export * from './topology.js';
+export * from './surfaceGeometry.js';
+export * from './generators.js';
+export * from './refinement.js';
+export * from './shellFrameCoupling.js';
+export * from './projectAdapter.js';
+
+export const MESH_SURFACE_ENGINE_VERSION='0.35.0-exp';

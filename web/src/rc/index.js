0b9080347f8f1e70c3d32ed509ea1f31bbbf2dc6
aaronkadima <aaronkadima@gmail.com>
2026-09-13 19:14:41 -0300

    feat: export v0.37 nonlinear RC core

---
+++ web/src/rc/index.js
@@ -0,0 +1,8 @@
+export * from './concreteDamage1d.js';
+export * from './rebar1d.js';
+export * from './bondSlip1d.js';
+export * from './sectionResponse.js';
+export * from './bondLinkComponent.js';
+
+export const NONLINEAR_RC_CONTRACT='nonlinear-rc/v1';
+export const NONLINEAR_RC_VERSION='0.37.0-exp';

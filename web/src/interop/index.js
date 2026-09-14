b28911fe4679be2baa65978dabe79f2680470c1a
aaronkadima <aaronkadima@gmail.com>
2026-09-13 21:36:57 -0300

    feat: expose v0.45 IFC interoperability API

---
+++ web/src/interop/index.js
@@ -0,0 +1,5 @@
+export {
+  IFC_INTEROP_CONTRACT,IFC_INTEROP_VERSION,IFC_SCHEMA,IFC_STANDARD,
+  ifcClassForElement,createIfcInteroperabilityModel,validateIfcInteroperabilityModel,
+  renderIfcInteroperabilityJson,parseIfcInteroperabilityJson,restoreStructuralCoreFromIfcModel,summarizeIfcInteroperability
+} from './ifc.js';

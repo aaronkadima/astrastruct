e1cba90d9f9f1a216d1c07363ffcaa2df21cbef2
aaronkadima <aaronkadima@gmail.com>
2026-09-14 01:13:23 -0300

    feat(vnl): expose v0.49 public API

---
+++ web/src/vnl/index.js
@@ -0,0 +1,25 @@
+export {
+  VNL_CONTRACT,
+  VNL_VERSION,
+  VNL_EXECUTION_CONTRACT,
+  VNL_PROJECT_CONTRACT,
+  VNL_PORT_TYPES,
+  VNL_BLOCK_CATALOG,
+  VNL_BLOCKS,
+  vnlBlockDefinition,
+  defaultVnlParams,
+} from './contracts.js';
+
+export {
+  createVnlNode,
+  createVnlEdge,
+  defaultGraph,
+  normalizeGraph,
+  validateGraph,
+  topologicalOrder,
+  connectSequentially,
+} from './graph.js';
+
+export { VnlCompileError, compileVnlGraph } from './compiler.js';
+export { executeVnlGraph } from './runtime.js';
+export { vnlGraphFromProject, withVnlGraph, clearVnlGraph } from './project.js';

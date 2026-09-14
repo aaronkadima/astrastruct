61bacb2bcec1a035d8577a5dcbf8418d82d9b96b
aaronkadima <aaronkadima@gmail.com>
2026-09-14 00:21:28 -0300

    feat: export IFC structural load mapping v0.48

--- web/src/interop/index.js
+++ web/src/interop/index.js
@@ -27,6 +27,9 @@ export {
   IFC_MATERIAL_STRENGTH_CONTRACT,IFC_MATERIAL_STRENGTH_VERSION,extractIfcStrengthMaterialProperties
 } from './ifcStrength.js';
 export {
+  IFC_LOAD_EXCHANGE_CONTRACT,IFC_LOAD_EXCHANGE_VERSION,createIfcStructuralLoadMapping,validateIfcStructuralLoadMapping,summarizeIfcStructuralLoadMapping
+} from './ifcLoads.js';
+export {
   IFC_STEP_OWNER_CONTRACT,IFC_STEP_OWNER_VERSION,validateIfcOwnerMetadata,emitIfcStepOwner
 } from './ifcStepOwner.js';
 export {
